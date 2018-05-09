3f35c5620271f7745a63c49eae659b697b699f90
Mark Suzuki <markdsuzuki@gmail.com>
2018-05-09 07:43:44 -0700

    functionality complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const HangMan = ({ image }) => {
+  return (
+    <div>
+      <div>
+        <img src={image[0]} />
+      </div>
+      <div>
+        <img src={image[1]} />
+      </div>
+      <div>
+        <img src={image[2]} />
+      </div>
+      <div>
+        <img src={image[3]} />
+      </div>
+      <div>
+        <img src={image[4]} />
+      </div>
+    </div>
+  );
+};
+
 const Word = ({ letter }) => {
   return <div className="item">{letter}</div>;
 };
@@ -38,6 +60,7 @@ class UserInputForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <input
           name="guess"
+          type="text"
           value={this.state.guess}
           onChange={this.handleChange}
           maxLength="1"
@@ -57,13 +80,21 @@ class App extends Component {
       word: this.props.word[0],
       wordMap: Array(this.props.word[0].length).fill(),
       guessArr: [],
-      gameOver: false
+      gameOver: false,
+      images: [
+        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350",
+        "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
+        "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
+        "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
+        "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
+      ]
     };
     this.handleAdd = this.handleAdd.bind(this);
     this.updateWordMap = this.updateWordMap.bind(this);
     this.checkWinner = this.checkWinner.bind(this);
   }
 
+  // Not Very Functional how can I imporve this?
   updateWordMap(word, wordMap, letter) {
     for (let i = 0; i < word.length; i++) {
       if (word[i] === letter) {
@@ -73,12 +104,15 @@ class App extends Component {
     return wordMap;
   }
 
+  //
   checkWinner(word, wordMap) {
     let wordd = word.replace(/\s+/g, "");
     let wordGuess = wordMap.join("").replace(/\s+/g, "");
     return wordd === wordGuess;
   }
 
+  // Is this to much logic for handling add
+  // should I break this out
   handleAdd(letter) {
     let newState = { ...this.state };
     if (newState.guessArr.indexOf(letter.guess.toLowerCase()) >= 0) {
@@ -108,6 +142,7 @@ class App extends Component {
     }
   }
 
+  // How Can I clean up
   resetGame() {
     let resetState = { ...this.state };
     resetState.guesses = 5;
@@ -128,6 +163,7 @@ class App extends Component {
         : this.state.gameOver
           ? "You Won"
           : "GameOver";
+    // Is this proper place to keep track of gameStatus
     if (this.state.gameOver) {
       return (
         <div className="App">
@@ -143,6 +179,12 @@ class App extends Component {
           <h1>Hangman</h1>
           <p>{status}</p>
           <UserInputForm handleAdd={this.handleAdd} />
+          <HangMan
+            image={this.state.images.slice(
+              0,
+              this.state.images.length - this.state.guesses
+            )}
+          />
           <div className="container">{word}</div>
         </div>
       );
